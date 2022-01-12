6abbb12f25609913ac7e07fd4d4ebee8ac98c385
nakai <shuta.nakai@hakuhodo-ms.co.jp>
2022-01-12 17:50:11 +0900

    fix liff.init

--- ar.js
+++ ar.js
@@ -83,12 +83,14 @@ AFRAME.registerComponent('duck', {
             'click',
             () => {
                 let userName;
-                liff.getProfile().then((profile) => {
-                    userName = profile.displayName;
-                    console.log(profile);
-                    console.log(userName);
+                liff.init({ liffId: '1656788750-0YlgEZw2' }).then(() => {
+                    liff.getProfile().then((profile) => {
+                        userName = profile.displayName;
+                        console.log(profile);
+                        console.log(userName);
+                    });
+                    location.href = `./get.html?id=duck&name=${userName}`;
                 });
-                // location.href = `./get.html?id=duck&name=${userName}`;
             },
             false
         );
