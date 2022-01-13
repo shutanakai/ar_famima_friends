3b8e2a12334223d4ae13a8da18bb89c71ad66d1f
nakai <shuta.nakai@hakuhodo-ms.co.jp>
2022-01-13 17:12:34 +0900

    check particle #11

--- webpack/aframe.js
+++ webpack/aframe.js
@@ -85,55 +85,6 @@ class Aframe {
             }
         });
 
-        AFRAME.registerComponent('star', {
-            // schema: {},
-
-            init: function () {
-                this.el.addEventListener(
-                    'click',
-                    () => {
-                        liff.init({ liffId: process.env.LIFF_ID }).then(() => {
-                            liff.getProfile().then((profile) => {
-                                location.href = `./get.html?id=deer&name=${profile.displayName}`;
-                            });
-                        });
-                    },
-                    false
-                );
-
-                this.el.addEventListener(
-                    'click',
-                    () => {
-                        liff.init({ liffId: process.env.LIFF_ID }).then(() => {
-                            liff.getProfile().then((profile) => {
-                                location.href = `./get.html?id=deer&name=${profile.displayName}`;
-                            });
-                        });
-                    },
-                    false
-                );
-
-                this.el.addEventListener(
-                    'raycaster-intersected',
-                    () => {
-                        this.el.setAttribute(
-                            'particle-system',
-                            'preset: default; size: 4; texture: ./images/texture_star.png; color: #fffead'
-                        );
-                    },
-                    false
-                );
-
-                this.el.addEventListener(
-                    'raycaster-intersected-cleared',
-                    () => {
-                        this.el.setAttribute('particle-system', '');
-                    },
-                    false
-                );
-            }
-        });
-
         document.addEventListener('load', function () {
             const canGetUserMediaVersion = 14.3;
             if (liff.getOS() === 'ios' && parseFloat(liff.getLineVersion()) < canGetUserMediaVersion) {
